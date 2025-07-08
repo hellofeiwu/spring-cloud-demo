ee210978e89ff548b18f5972b5bb672670f39e37
hellofeiwu <hellofeiwu@gmail.com>
2025-07-08 21:34:46 +0800

    add 测试过程代码

--- config/config-client-eureka/src/main/java/com/imooc/springcloud/ConfigClientEurekaApplication.java
+++ config/config-client-eureka/src/main/java/com/imooc/springcloud/ConfigClientEurekaApplication.java
@@ -6,7 +6,9 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
+// 这个不加都会被注册到注册中心去，因为配置文件中配置 配置中心 微服务 名字的时候
+// 用到了 eureka server 的 discovery 这个功能
 public class ConfigClientEurekaApplication {
     public static void main(String[] args) {
         new SpringApplicationBuilder(ConfigClientEurekaApplication.class)
